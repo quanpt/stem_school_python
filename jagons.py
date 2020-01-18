784b47ac35f61b559ef6b29fe6d7960c5c11ea9f
Quan Pham <quanpt@gmail.com>
2020-01-18 20:48:58 +1100

    Added lesson 1

---
+++ jagons.py
@@ -0,0 +1,4 @@
+program='''A computer program is a collection of instructions that tell a computer to perform a specific task.'''
+memory='''Edison's microchip has the ability to store some things, like programs. These programs are stored in the robot's memory. We can tell Edison which of these programs we want to run by driving over special barcodes.'''
+hardware='''All computers have two main parts: hardware and software. Hardware is the physical parts of a computer (or robot).'''
+software='''Software is the set of programs and applications that make hardware, like a computer or a robot, run.'''
